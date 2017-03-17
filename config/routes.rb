d834dade52b8b870f8a3627ea8274743d5efb29c
hiroki nakakita <ikigairokky@gmail.com>
2017-03-17 15:43:19 +0000

    Display a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
-  devise_for :users
-   root  'static_pages#home'
- match '/',    to: 'static_pages#home',    via: 'get' 
+  
+ devise_for :users 
+  resources :users, only: [:show]
+  
+  root  'static_pages#home'
+  match '/',    to: 'static_pages#home',    via: 'get' 
 match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
