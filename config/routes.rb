e5e11c6f18c6f11925676b9caecc15920339c6af
hiroki nakakita <ikigairokky@gmail.com>
2017-03-21 07:03:56 +0000

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 SampleApp::Application.routes.draw do
-  
- devise_for :users 
+   devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
   resources :users, only: [:show]
-  
   root  'static_pages#home'
   match '/',    to: 'static_pages#home',    via: 'get' 
 match '/help',    to: 'static_pages#help',    via: 'get'
